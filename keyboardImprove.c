ca75c5b6778a73df5c9737c8c0a914a994bed383
FransLukas <franslukas@gmail.com>
2017-12-22 17:02:30 +0100

    Super pseudo code

--- keyboardImprove.c
+++ keyboardImprove.c
@@ -6,6 +6,11 @@
  * keystrokes you use the most.
  */
 int main(int argc, char *argv[]){
-
-
+    /* Creating a demon
+    * 1. Fork
+    * 2. setsid to create a new session
+    * 3. change current directory to root.
+    * 4. set file mode creation mask to 0
+    * 5. Close all fildescriptors that will not be used.
+    */
 }
---
+++ keyboardImprove.c~
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+/*
+ * A program that will run in background and record what 
+ * keystrokes you use the most.
+ */
+int main(int argc, char *argv[]){
+
+
+}
